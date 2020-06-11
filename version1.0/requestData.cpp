26ea5350ba6f1b1c77e24730b8b3c0a554236173
xiaogouaiyaotou <47123798+xiaogouaiyaotou@users.noreply.github.com>
2020-06-11 17:57:18 -0500

    Update requestData.cpp

--- version1.0/requestData.cpp
+++ version1.0/requestData.cpp
@@ -10,10 +10,6 @@
 #include <sys/mman.h>
 #include <queue>
 
-#include <opencv/cv.h>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/opencv.hpp>
 using namespace cv;
 
 //test
